a53a8a8eb56f3af5f2624edad58965a8df9fe6e2
Kundan Kumar Jaiswal <jaiswal.k.kundan@gmail.com>
2025-06-23 21:31:21 +0530

    Updating Codes -> js-basics

---
+++ js-basics/script.js
@@ -0,0 +1,18 @@
+alert("Let's begain our JS Journey");
+
+console.log("Welcome! to the console of my first JavaScript.");
+
+fullName = "Kundan K Jaiswal";
+console.log("Name :", fullName);
+age = 20;;
+console.log("Age :", age);
+
+x = null;
+y = undefined;
+console.log(x);
+console.log(y);
+
+isFollow = true;
+isSubscribe = false;
+console.log(isFollow);
+console.log(isSubscribe)
