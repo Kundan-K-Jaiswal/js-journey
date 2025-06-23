9c39e62170cad5b0c1d48c0c324cf386119732e9
Kundan Kumar Jaiswal <jaiswal.k.kundan@gmail.com>
2025-06-23 21:46:47 +0530

    Updating Codes -> js-object

---
+++ js-object/script.js
@@ -0,0 +1,35 @@
+const fullName = "Kundan K Jaiswal";
+let age = 20;
+let myStatus = "Single";
+myStatus = "Mingle";
+
+console.log(fullName);
+console.log(age);
+console.log(myStatus)
+
+
+let x = BigInt(123);
+let y = Symbol("hey!!!");
+
+console.log(x);
+console.log(y);
+
+console.log(typeof x);
+console.log(typeof y);
+
+const student = {
+  fullName : "Kundan Kumar Jaiswal",
+  age : 20,
+  sex : "male",
+};
+
+console.log(student);
+console.log(student["age"]);
+
+
+console.log(1+1);
+console.log('1'+1);
+console.log(1-1);
+console.log('1'-1);
+console.log('1'/1);
+console.log('1'*1);
