74478e2b9a5018c432947022d9167e77d8e59f24
Kundan Kumar Jaiswal <jaiswal.k.kundan@gmail.com>
2025-06-24 18:05:27 +0530

    Solving The Practice Questions -> object-questions

---
+++ object-questions/script.js
@@ -0,0 +1,37 @@
+// Question Number 01
+const product = {
+  productName: "Parker Jotter Standard CT Ball Pen (Black)",
+  reviewNumber: 7002,
+  currency: Symbol("$"),
+  price: 270,
+  mrp: 285,
+  discountPercentage: 5,
+};
+
+console.log(product);
+console.log(product["price"]);
+console.log(product["mrp"]);
+
+
+
+
+// Question Number 02
+const profile = {
+  userName: "shardhakhapra",
+  posts: 195,
+  followers: 569000,
+  following: 4,
+  isFollow: false,
+  ismessage: false,
+  Name: "Shradha Khapra",
+  handelName: "@shradhakhapra",
+  description: "Enterpreneur",
+  about: "Apna College | Ex-Microsoft, DRDO",
+  quote: "To educate someone is the highest privilege",
+};
+
+console.log(profile);
+console.log(profile["isFollow"]);
+console.log(profile["quote"]);
+
+
