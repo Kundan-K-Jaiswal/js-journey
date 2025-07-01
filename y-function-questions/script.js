2a74e69b0bf1d18dbfaa65e31a51260602c488fc
Kundan Kumar Jaiswal <jaiswal.k.kundan@gmail.com>
2025-07-01 07:14:53 +0530

    Fixing Question Number 3 -> functon-questions

--- y-function-questions/script.js
+++ y-function-questions/script.js
@@ -1,6 +1,7 @@
 // Solution of the questions releated to functions...
 
 // Question 1: Write a function to return vowel number form a given string.
+console.log("Question 1");
 function vowelNumber(word) {
   word = word.toLowerCase();
   let count = 0;
@@ -22,6 +23,7 @@ console.log("\n");
 
 
 //Question 2: Write an arrow function to return vowel number form a given string.
+console.log("Question 2");
 const vowelCount = (word) => {
   word = word.toLowerCase();
   let count = 0;
@@ -42,12 +44,14 @@ console.log("\n");
 
 
 //Question 3: Find the sum of square of numbers of array using forEach loop.
+console.log("Question 3");
 let numbersArray = [10,12,13,14,15];
 let numberSum = 0;
 console.log("Given array :", numbersArray);
 numbersArray.forEach((item)=>{
   numberSum += item**2;
 });
+console.log("The sum of square of numbers is :", numberSum);
 console.log("\n");
 
 
@@ -55,6 +59,7 @@ console.log("\n");
 
 
 //Question 4: Collect the students marks more than 90 in an array.
+console.log("Question 4");
 let marks = [37,12,93,55,62,18,99,94,86,89];
 let goodMarks = marks.filter((val)=>{
   return val>=90;
@@ -68,6 +73,7 @@ console.log("\n");
 
 
 //Question 5: Find the sum and product using reduce method.
+console.log("Question 5");
 let userNum = prompt("Enter a number to calculate sum and product till that number :");
 let arr = [];
 for ( let i=1; i<= userNum; i++) {
